7117bee3aebc8e8e4fa51f35b9f0b89563306e34
ProgrammerSuperPRO-2 <137902118+ProgrammerSuperPRO-2@users.noreply.github.com>
2023-06-27 10:25:01 -0300

    Add files via upload

---
+++ ex065.py
@@ -0,0 +1,16 @@
+valores = []
+while True:
+    num = int(input("Digite um número : "))
+    valores.append(num)
+    mínimo = min(valores)
+    máximo = max(valores)
+    confirm=input("Continuar : ")
+    if confirm.lower().strip() == "s":
+        continue
+    elif confirm.lower().strip() == "n":
+        break
+print(f"O maior valor é : {máximo}")
+print(f"O menor valor é : {mínimo}")
+
+
+
